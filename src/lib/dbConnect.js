0b86607218ee7135893dfa5d7c12953aa09bba38
naderouali <oualinader@gmail.com>
2024-10-11 12:31:07 +0100

    auth

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -7,15 +7,26 @@ if (!MONGODB_URI) {
 }
 
 let cached = global.mongoose;
+
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function dbConnect() {
-  if (cached.conn) return cached.conn;
+  if (cached.conn) {
+    return cached.conn;
+  }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then(mongoose => mongoose);
+    const options = {
+      bufferCommands: false, 
+      useNewUrlParser: true, 
+      useUnifiedTopology: true,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, options).then((mongoose) => {
+      return mongoose;
+    });
   }
   cached.conn = await cached.promise;
   return cached.conn;
